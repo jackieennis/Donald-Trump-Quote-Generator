499541fedcf0ae225d46dcb5377f1169040cc3d0
Jackie Ennis <jacqueline.ennis042@gmail.com>
2016-07-07 14:36:00 -0400

    Added functionality
    Randomly generates quotes, resized button image, added title

--- DJTQuoteGenerator/ViewController.swift
+++ DJTQuoteGenerator/ViewController.swift
@@ -11,19 +11,50 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
-    var bringingDrugs = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bringingDrugs", ofType: "mp3")!)
+    var quote = NSURL()
     
     var buttonAudioPlayer = AVAudioPlayer()
+    
+    func generateRandomQuote() -> String {
+        let randPicker = Int(arc4random_uniform(14))
+        var currentQuote = String()
+        switch randPicker {
+        case 1:
+            currentQuote = "nowhere"
+        case 2:
+            currentQuote = "mexican"
+        case 3:
+            currentQuote = "love"
+        case 4:
+            currentQuote = "knees"
+        case 5:
+            currentQuote = "ladies"
+        case 6:
+            currentQuote = "pay"
+        case 7:
+            currentQuote = "missUSA"
+        case 8:
+            currentQuote = "muslim"
+        case 9:
+            currentQuote = "wall"
+        case 10:
+            currentQuote = "loan"
+        case 11:
+            currentQuote = "rapists"
+        case 12:
+            currentQuote = "tremendous"
+        case 13:
+            currentQuote = "univision"
+        default:
+            currentQuote = "rapists"
+        }
+        return currentQuote
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        do {
-            try buttonAudioPlayer = AVAudioPlayer(contentsOfURL: bringingDrugs)
-            buttonAudioPlayer.play()
-        } catch {
-            print("Cannot play file because \(error)")
-        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,8 +63,19 @@ class ViewController: UIViewController {
     }
 
     @IBAction func playAudio(sender: AnyObject) {
+        let quotePicked = self.generateRandomQuote()
+        quote = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(quotePicked, ofType: "mp3")!)
+        do {
+            try buttonAudioPlayer = AVAudioPlayer(contentsOfURL: quote)
+            if(buttonAudioPlayer.playing == true)
+            {
+                buttonAudioPlayer.stop()
+            }
+            buttonAudioPlayer.play()
+        } catch {
+            print("Cannot play audio)")
+        }
+
     }
-    
-    
 }
 
